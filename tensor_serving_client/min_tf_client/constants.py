cda3aaee1a59a2cefb3978441ceda1a4ed3886dc
Soon-Ee Cheah <scheah@zendesk.com>
2019-12-27 16:00:18 +1100

    add types test

--- tensor_serving_client/min_tf_client/constants.py
+++ tensor_serving_client/min_tf_client/constants.py
@@ -14,18 +14,18 @@ NP_TO_TF_MAPPING = {
     np.float16: TFType(TFDType="DT_HALF", TensorProtoField="half_val"),
     np.float32: TFType(TFDType="DT_FLOAT", TensorProtoField="float_val"),
     np.float64: TFType(TFDType="DT_DOUBLE", TensorProtoField="double_val"),
+    np.int8: TFType(TFDType="DT_INT8", TensorProtoField="int_val"),
+    np.int16: TFType(TFDType="DT_INT16", TensorProtoField="int_val"),
     np.int32: TFType(TFDType="DT_INT32", TensorProtoField="int_val"),
+    np.int64: TFType(TFDType="DT_INT64", TensorProtoField="int64_val"),
     np.uint8: TFType(TFDType="DT_UINT8", TensorProtoField="int_val"),
     np.uint16: TFType(TFDType="DT_UINT16", TensorProtoField="int_val"),
     np.uint32: TFType(TFDType="DT_UINT32", TensorProtoField="uint32_val"),
     np.uint64: TFType(TFDType="DT_UINT64", TensorProtoField="uint64_val"),
-    np.int16: TFType(TFDType="DT_INT16", TensorProtoField="int_val"),
-    np.int8: TFType(TFDType="DT_INT8", TensorProtoField="int_val"),
-    np.str_: TFType(TFDType="DT_STRING", TensorProtoField="string_val"),
     np.complex64: TFType(TFDType="DT_COMPLEX64", TensorProtoField="scomplex_val"),
     np.complex128: TFType(TFDType="DT_COMPLEX128", TensorProtoField="dcomplex_val"),
-    np.int64: TFType(TFDType="DT_INT64", TensorProtoField="int64_val"),
-    np.bool: TFType(TFDType="DT_BOOL", TensorProtoField="bool_val"),
+    np.str_: TFType(TFDType="DT_STRING", TensorProtoField="string_val"),
+    np.bool_: TFType(TFDType="DT_BOOL", TensorProtoField="bool_val"),
 }
 
 TF_TO_NP_MAPPING = {v.TFDType: k for k, v in NP_TO_TF_MAPPING.items()}
@@ -46,5 +46,5 @@ NUMERICAL_TYPES = {
     np.complex64,
     np.complex128,
     np.int64,
-    np.bool
+    np.bool_,
 }
