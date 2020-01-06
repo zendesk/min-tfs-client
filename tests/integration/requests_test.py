64160c0db77e83da567d61648e30f3391fc2e3ca
Soon-Ee Cheah <scheah@zendesk.com>
2020-01-06 14:58:15 +1100

    formatting

--- tests/integration/requests_test.py
+++ tests/integration/requests_test.py
@@ -1,17 +1,14 @@
-from min_tf_client.requests import TensorServingClient
-from min_tf_client.tensors import tensor_proto_to_ndarray
-from pytest import fixture
 import numpy as np
 from numpy.testing import assert_array_almost_equal, assert_array_equal
+from pytest import fixture
+
+from min_tf_client.requests import TensorServingClient
+from min_tf_client.tensors import tensor_proto_to_ndarray
 
 
 @fixture(scope="function")
 def unsecured_ts_client():
-    return TensorServingClient(
-        host="127.0.0.1",
-        port=4080,
-        credentials=None
-    )
+    return TensorServingClient(host="127.0.0.1", port=4080, credentials=None)
 
 
 def test_request(unsecured_ts_client):
@@ -22,18 +19,15 @@ def test_request(unsecured_ts_client):
         input_dict={
             "string_input": np.array(["hello world"]),
             "float_input": np.array([0.1], dtype=np.float32),
-            "int_input": np.array([2], dtype=np.int64)
-        }
+            "int_input": np.array([2], dtype=np.int64),
+        },
     )
     assert_array_almost_equal(
-        tensor_proto_to_ndarray(response.outputs["float_output"]),
-        np.array([0.1], dtype=np.float32)
+        tensor_proto_to_ndarray(response.outputs["float_output"]), np.array([0.1], dtype=np.float32)
     )
     assert_array_equal(
-        tensor_proto_to_ndarray(response.outputs["int_output"]),
-        np.array([2], dtype=np.int64)
+        tensor_proto_to_ndarray(response.outputs["int_output"]), np.array([2], dtype=np.int64)
     )
     assert_array_equal(
-        tensor_proto_to_ndarray(response.outputs["string_output"]),
-        np.array(["hello world"])
+        tensor_proto_to_ndarray(response.outputs["string_output"]), np.array(["hello world"])
     )
