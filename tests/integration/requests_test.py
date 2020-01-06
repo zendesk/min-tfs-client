3b70d470c4a6bb359f2249f2c17932c1bbfad5f6
Soon-Ee Cheah <scheah@zendesk.com>
2020-01-06 13:52:18 +1100

    add predict test

--- tests/integration/requests_test.py
+++ tests/integration/requests_test.py
@@ -1,5 +1,6 @@
 from min_tf_client.requests import TensorServingClient
 from pytest import fixture
+import numpy as np
 
 
 @fixture(scope="function")
@@ -12,4 +13,15 @@ def unsecured_ts_client():
 
 
 def test_request(unsecured_ts_client):
-    ...
+    client = unsecured_ts_client
+    response = client.predict_request(
+        model_name="default",
+        model_version=1,
+        input_dict={
+            "string_input": np.array(["hello world"]),
+            "float_input": np.array([0.1], dtype=np.float32),
+            "int_input": np.array([2], dtype=np.int64)
+        }
+    )
+    print(response)
+    assert response is not None
