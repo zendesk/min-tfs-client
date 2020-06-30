8e1d102a88deacc5bd57b01ddeae2b54a8345fec
John Shen <john@everops.com>
2020-06-30 15:57:54 -0700

    remove hardcoded travis home directory that does not exist in GA

--- scripts/travis/protoc_install.sh
+++ scripts/travis/protoc_install.sh
@@ -2,7 +2,7 @@
 
 PROTOBUF_VERSION=3.3.0
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-pushd /home/travis
+pushd $HOME
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
 unzip ${PROTOC_FILENAME}
 bin/protoc --version
