67e2db159140e97885e0c09823fb0be6cb2c56eb
Dana Ma <dma@zendesk.com>
2020-07-01 12:52:44 +1000

    what is even happening in this script

--- scripts/travis/protoc_install.sh
+++ scripts/travis/protoc_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -ex
 
 PROTOBUF_VERSION=3.3.0
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-pushd ${GITHUB_WORKSPACE}
+pushd ${HOME}
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
 unzip -o ${PROTOC_FILENAME} bin/protoc
 unzip -o ${PROTOC_FILENAME} 'include/*'
