4454be3e83004b4d0a8a8f902b029512a251f566
Dana Ma <dma@zendesk.com>
2020-07-01 12:34:28 +1000

    [fix] unzip protoc files to location on path

--- scripts/travis/protoc_install.sh
+++ scripts/travis/protoc_install.sh
@@ -2,8 +2,9 @@
 
 PROTOBUF_VERSION=3.3.0
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
-pushd $HOME
+pushd ${GITHUB_WORKSPACE}
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
-unzip ${PROTOC_FILENAME}
+unzip -o ${PROTOC_FILENAME} bin/protoc
+unzip -o ${PROTOC_FILENAME} 'include/*'
 bin/protoc --version
 popd
