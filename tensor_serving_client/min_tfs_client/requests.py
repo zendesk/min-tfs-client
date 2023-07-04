8a295a277c35f27af455d16b047bb17a01ae2fe4
Leo <leo.hoare@live.com>
2023-07-05 09:13:57 +1000

    add aysncio client

--- tensor_serving_client/min_tfs_client/requests.py
+++ tensor_serving_client/min_tfs_client/requests.py
@@ -19,15 +19,114 @@ RequestTypes = Union[PredictRequest, ClassificationRequest, RegressionRequest]
 ResponseTypes = Union[PredictResponse, ClassificationResponse, RegressionResponse]
 
 
-class TensorServingClient:
+class AsyncTensorServingClient:
     def __init__(
-        self, host: str, port: int, credentials: Optional[grpc.ssl_channel_credentials] = None,
+        self,
+        host: str,
+        port: int,
+        credentials: Optional[grpc.ssl_channel_credentials] = None,
+        options=None,
     ) -> None:
         self._host_address = f"{host}:{port}"
         if credentials:
-            self._channel = grpc.secure_channel(self._host_address, credentials)
+            self._channel = grpc.aio.secure_channel(target=self._host_address, credentials=credentials, options=options)
         else:
-            self._channel = grpc.insecure_channel(self._host_address)
+            self._channel = grpc.aio.insecure_channel(target=self._host_address, options=options)
+
+    async def _make_inference_request(
+        self,
+        model_name: str,
+        input_dict: Dict[str, np.ndarray],
+        request_pb: RequestTypes,
+        timeout: int,
+        model_version: Optional[int],
+    ) -> ResponseTypes:
+        stub = PredictionServiceStub(self._channel)
+        request = request_pb()
+        request.model_spec.name = model_name
+
+        if model_version is not None:
+            request.model_spec.version.value = model_version
+
+        for k, v in input_dict.items():
+            request.inputs[k].CopyFrom(ndarray_to_tensor_proto(v))
+        return await stub.Predict(request, timeout=timeout)
+
+    async def predict_request(
+        self,
+        model_name: str,
+        input_dict: Dict[str, np.ndarray],
+        timeout: int = 60,
+        model_version: Optional[int] = None,
+    ) -> PredictResponse:
+        request_params: Dict[str, Any] = {
+            "model_name": model_name,
+            "model_version": model_version,
+            "input_dict": input_dict,
+            "request_pb": PredictRequest,
+            "timeout": timeout,
+        }
+        return await self._make_inference_request(**request_params)
+
+    async def classification_request(
+        self,
+        model_name: str,
+        input_dict: Dict[str, np.ndarray],
+        timeout: int = 60,
+        model_version: Optional[int] = None,
+    ) -> ClassificationResponse:
+        request_params: Dict[str, Any] = {
+            "model_name": model_name,
+            "model_version": model_version,
+            "input_dict": input_dict,
+            "request_pb": ClassificationRequest,
+            "timeout": timeout,
+        }
+        return await self._make_inference_request(**request_params)
+
+    async def regression_request(
+        self,
+        model_name: str,
+        input_dict: Dict[str, np.ndarray],
+        timeout: int = 60,
+        model_version: Optional[int] = None,
+    ) -> RegressionResponse:
+        request_params: Dict[str, Any] = {
+            "model_name": model_name,
+            "model_version": model_version,
+            "input_dict": input_dict,
+            "request_pb": RegressionRequest,
+            "timeout": timeout,
+        }
+        return await self._make_inference_request(**request_params)
+
+    async def model_status_request(
+        self,
+        model_name: str,
+        model_version: Optional[int] = None,
+        timeout: Optional[int] = 10,
+    ) -> GetModelStatusResponse:
+        stub = ModelServiceStub(self._channel)
+        request = GetModelStatusRequest()
+        request.model_spec.name = model_name
+        if model_version:
+            request.model_spec.version.value = model_version
+        return await stub.GetModelStatus(request, timeout)
+
+
+class TensorServingClient:
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        credentials: Optional[grpc.ssl_channel_credentials] = None,
+        options=None,
+    ) -> None:
+        self._host_address = f"{host}:{port}"
+        if credentials:
+            self._channel = grpc.secure_channel(target=self._host_address, credentials=credentials, options=options)
+        else:
+            self._channel = grpc.insecure_channel(target=self._host_address, options=options)
 
     def _make_inference_request(
         self,
